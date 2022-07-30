7db729cca47489eba8deae29740ff0d1c1875bce
Samiul Fahad <samiul_fahad@yahoo.com>
2022-07-30 20:02:23 +0600

    Solved

--- src/index.js
+++ src/index.js
@@ -5,19 +5,22 @@ const app = express();
 app.use(express.json())
 const port = process.env.PORT || 3000
 
-const instruction = `
-endpoint /sign-up (POST) to create a user with email and password field.
-endpoint /login (POST) to login using a valid email and password. A token will be provided.
-endpoint /profile (GET) to read own profile
-endpoint /profile (PATCH) to update profile
-endpoint /profile/avatar (POST/GET/DELETE, ##field name=> avatar) to interact with profile Picrure
-endpoint /profile/delete (DELETE) to delete own profile
-endpoint /logout (POST) to logout and delete a jwt token
-endpoint /logoutall (POST) to expire all active sessions
-endpoint /task (POST) to create a task with Title, Description and Completed (Boolean) field
-endpoint /task/:id (POST) to update a Task
-endpoint /task/:id (GET/DELETE) to read and delete a task
-endpoint /tasks (GET) to read all tasks of the logged in user`
+const instruction = `<h3 style="text-color:green; margin-top:50px; margin-left:200px">Use Postman to interact with the API. A JWT token will be provided to authorise certain routes.</h3>
+<p style="text-color:green; margin-left:200px">
+<b>Technologies Used:</b> Node.js, Express.js, MongoDB, Mongoose, JSON Web Token, Bcrypt.js, Multer and more
+</p>
+<h4 style="margin-left:200px">endpoint /sign-up (POST) to create a user with email and password field<br>
+endpoint /login (POST) to login using a valid email and password. A token will be provided <br>
+endpoint /profile (GET) to read own profile<br>
+endpoint /profile (PATCH) to update profile<br>
+endpoint /profile/avatar (POST/GET/DELETE, ##field name=> avatar) to interact with profile Picrure<br>
+endpoint /profile/delete (DELETE) to delete own profile<br>
+endpoint /logout (POST) to logout and delete a jwt token<br>
+endpoint /logoutall (POST) to expire all active sessions<br>
+endpoint /task (POST) to create a task with Title, Description and Completed (Boolean) field<br>
+endpoint /task/:id (POST) to update a Task<br>
+endpoint /task/:id (GET/DELETE) to read and delete a task<br>
+endpoint /tasks (GET) to read all tasks of the logged in user</h4>`
 
 
 // Adding User Router
